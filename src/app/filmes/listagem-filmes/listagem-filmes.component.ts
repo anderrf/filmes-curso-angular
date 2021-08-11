4e4919eac4f0a6b1080c0ef9680234bf086ae078
anderrf <anderson_rocha_farias@outlook.com>
2021-08-11 00:13:18 -0300

    Criação do formulário de pesquisa de filmes

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { FilmesService } from './../../core/filmes.service';
 import { Component, OnInit } from '@angular/core';
 import { Filme } from 'src/app/shared/models/filme';
@@ -12,10 +13,17 @@ export class ListagemFilmesComponent implements OnInit {
   readonly qtdPagina = 4;
   pagina = 0;
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+    this.generos = ['Ação', 'Aventura', 'Comédia', 'Drama', 'Ficção científica', 'Romance', 'Terror'];
     this.listarFilmes();
   }
 

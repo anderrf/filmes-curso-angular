f5f599276dd8689c1e4118810626ad44b9cfe48a
anderrf <anderson_rocha_farias@outlook.com>
2021-08-11 15:00:05 -0300

    Melhora no layout de card de filme, ng-template em caso de registros não encontrados, uso de debounceTime para melhora de performance

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -2,6 +2,7 @@ import { ConfigParams } from './../../shared/models/config-params';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { FilmesService } from './../../core/filmes.service';
 import { Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
 import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
@@ -11,6 +12,7 @@ import { Filme } from 'src/app/shared/models/filme';
 })
 export class ListagemFilmesComponent implements OnInit {
 
+  readonly semFoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg';
   config: ConfigParams = {
     pagina: 0,
     limite: 4
@@ -26,14 +28,18 @@ export class ListagemFilmesComponent implements OnInit {
       texto: [''],
       genero: ['']
     });
-    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
-      this.config.pesquisa = val;
-      this.resetarConsulta();
-    });
-    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
-      this.config.campo = {tipo: 'genero', valor: val};
-      this.resetarConsulta();
-    });
+    this.filtrosListagem.get('texto').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.pesquisa = val;
+        this.resetarConsulta();
+      });
+    this.filtrosListagem.get('genero').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.campo = {tipo: 'genero', valor: val};
+        this.resetarConsulta();
+      });
     this.generos = ['Ação', 'Aventura', 'Comédia', 'Drama', 'Ficção científica', 'Romance', 'Terror'];
     this.listarFilmes();
   }
